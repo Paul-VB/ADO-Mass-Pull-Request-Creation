aecbe809d6c96d59c41bb3721b86b9159dd335e4
Paul Vanden Broeck <I823914@ngic.com>
2022-08-04 14:40:55 -0400

    adding a thing to let me easily copy the contents of this repo to a subfolder in a nother local repo

---
+++ CopyToBiggerRepo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+declare fileThatHoldsTargetPath="documentationFolderPath.txt"
+declare targetPath=`cat $fileThatHoldsTargetPath`
+declare tempFolderName="TemporaryFolder_dsuifgh5d"
+eval "git clone ./ '$tempFolderName'"
+eval "cp -r \"$tempFolderName/.\" \"$targetPath\\\\\""
+eval "rm -R -f '$tempFolderName'"
+eval "rm -R -f '$targetPath'/.git"
+eval "rm -R -f '$targetPath'/.gitignore"
+eval "rm -R -f '$targetPath'/`basename "$0"`"
+
+read -r -p "Press the any key to continue " input
