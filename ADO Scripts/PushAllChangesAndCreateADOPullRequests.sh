3227f90033a28dcdf10eedf570213e5d087e78a0
Paul Vanden Broeck <I823914@ngic.com>
2022-08-04 16:36:37 -0400

    well i got it to read and save what the user types to a variable....

--- ADO Scripts/PushAllChangesAndCreateADOPullRequests.sh
+++ ADO Scripts/PushAllChangesAndCreateADOPullRequests.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-declare scriptPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
-declare currDate=`date +"%Y-%m-%d_%H-%M-%S"`
-
-#for all the repos we push to, this will be the source branch of our PR
-declare sourceBranchName = getCliAgrument $1 "Please Enter your source branch name: "
-
-#this will be what we use as the commit message for all our git commits
-declare commitMessage = getCliAgrument $2 "Please enter your commit message:"
-
-echo "source branch name: $sourceBranchNameStripped"
-echo "commit message: $commitMessage"
-
-#now we analyze all the folders in the git base folder to see which of them are actually git repos
-
-#waits for the user to press the any key
-read -r -p "Press the any key to continue " input
 
 #given a cli agrument variable, return that variable if it was supplied.
 #If it was not, prompt the user to enter it with a custom message
@@ -25,7 +9,23 @@ function getCliAgrument(){
         return $1
     else
         read -p "$2" result
-        return result
+        echo $result
     fi 
 
 }
+
+declare scriptPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
+declare currDate=`date +"%Y-%m-%d_%H-%M-%S"`
+
+sourceBranchNameStripped=$(getCliAgrument $1 "\"Please_Enter_your_source_branch_name:_\"")
+#this will be what we use as the commit message for all our git commits
+commitMessage=$(getCliAgrument $2 "\"Please enter your commit message: \"")
+
+echo "the source branch name is: $sourceBranchNameStripped"
+echo "the commit message is: $commitMessage"
+
+#now we analyze all the folders in the git base folder to see which of them are actually git repos
+
+#waits for the user to press the any key
+read -r -p "Press the any key to continue " input
+
