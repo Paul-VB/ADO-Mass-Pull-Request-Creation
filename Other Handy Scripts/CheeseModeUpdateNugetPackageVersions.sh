2f8b0fe4880ad9eb2f8f6e00439287ea4341327e
Paul Vanden Broeck <I823914@ngic.com>
2022-08-24 17:12:41 -0400

    performance improvements

--- Other Handy Scripts/CheeseModeUpdateNugetPackageVersions.sh
+++ Other Handy Scripts/CheeseModeUpdateNugetPackageVersions.sh
@@ -4,11 +4,11 @@
 readarray -t nugetPackages < "./NuGetPackages.txt"
 
 
-gitRoot="C:\git\PL";
+gitRoot="C:\git";
 
 newPackageVersion="3.0.170"
 
-eval cd \"$gitRoot\" || { true; };
+
 echo "replacing old version numbers with new version numbers. this might take a minute..."
 
 function overwriteNuGetPackageVersion(){
@@ -16,11 +16,29 @@ function overwriteNuGetPackageVersion(){
     local newVersion="${2}"
     local thingToMatch="(<PackageReference Include=\"${nuGetPackageName}\">.{1,}?<Version>)[\.\d]{1,}?(<\/Version>)"
     local thingToChangeItTo="\${1}${newVersion}\$2"
-    find . -type f -name '*.[vc][bs]proj' -exec perl -w -i -p -00e "s/${thingToMatch}/${thingToChangeItTo}/gs" {} \;
+    find \. -type f -name '*.[vc][bs]proj' -exec perl -w -i -p -00e "s/${thingToMatch}/${thingToChangeItTo}/gs" {} \;
 }
 
-for currNugetPackage in "${nugetPackages[@]}" ; do
-    currNugetPackage=$(echo "${currNugetPackage}" | sed -E 's/\r//g')
-    echo -e "trying to update ${currNugetPackage} to version ${newPackageVersion}";
-    overwriteNuGetPackageVersion "${currNugetPackage}" "${newPackageVersion}" &
-done
+function updateAllRepos(){
+    eval cd \"$gitRoot\" || { true; };
+    for currDirectory in */ ; do 
+        currDirectory=${currDirectory::-1} #this strips off the trailing /
+        eval cd \"$currDirectory\" || { true; };
+        if [ -d .git ]; then
+            echo "Updating repo: $currDirectory"
+            #we know we're in a git repo. Does the git repo have unmerged changes?
+            for currNugetPackage in "${nugetPackages[@]}" ; do
+                currNugetPackage=$(echo "${currNugetPackage}" | sed -E 's/\r//g')
+                overwriteNuGetPackageVersion "${currNugetPackage}" "${newPackageVersion}" &
+            done
+        fi
+        wait
+        cd ..; 
+    done 
+    wait
+}
+
+updateAllRepos
+echo "finished"
+
+
