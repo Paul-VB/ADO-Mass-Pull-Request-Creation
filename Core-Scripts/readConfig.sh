7e4f8cabeda0d9eff12d6a1a31d74414ed4710fa
Paul Vanden Broeck <I823914@ngic.com>
2022-08-26 15:57:42 -0400

    bug fixes

--- Core-Scripts/readConfig.sh
+++ Core-Scripts/readConfig.sh
@@ -3,13 +3,13 @@ declare scriptPath
 scriptPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )";
 
 #this is where the main config file should live
-configFilePath="../config.cfg"
+configFilePath="${scriptPath}/../config.cfg"
 
 #check if the config file exists
 if [[ -f ${configFilePath} ]]; then
     #init the config variables
-    source "$scriptPath/../config.cfg"
+    source "$configFilePath"
 else
-    echo "Config file does not exist. Please run initConfig.sh"
+    echo "Cannot find config file at: \"$configFilePath\" . Please run initConfig.sh"
     exit 1
 fi
