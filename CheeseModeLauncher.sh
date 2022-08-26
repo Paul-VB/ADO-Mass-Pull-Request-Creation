7ae9dbe1fe28bd190d4622ea9e1da8fe66c53b32
Paul Vanden Broeck <I823914@ngic.com>
2022-08-26 15:40:05 -0400

    adding a press the any key if the cheesemode launcher cant find the nugetPackageFile

--- CheeseModeLauncher.sh
+++ CheeseModeLauncher.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+declare scriptPath
+scriptPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )";
+
+#press the any key
+source "$scriptPath/Core-Scripts/pressAnyKeyToContinue.sh"
 
 nugetPackageVersionsFilePath="./NuGetPackages.txt"
+
+
 
 #check if the nugetPackageVersionsFile exists
 if [[ -f ${nugetPackageVersionsFilePath} ]]; then
     sh 'Core-Scripts/CheeseModeUpdateNugetPackageVersions.sh' "$(<${nugetPackageVersionsFilePath})"
 else
     echo "${nugetPackageVersionsFilePath} does not exist."
+    pressAnyKeyToContinue
 fi
