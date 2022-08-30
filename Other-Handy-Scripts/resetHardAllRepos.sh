4624d699a7d0c105cffb392a5fa2f60f65206dd1
Paul Vanden Broeck <I823914@ngic.com>
2022-08-30 10:49:40 -0400

    moving the reset code to a different function so you can pull each as well if you need to

--- Other-Handy-Scripts/resetHardAllRepos.sh
+++ Other-Handy-Scripts/resetHardAllRepos.sh
@@ -10,13 +10,19 @@ function getCurrentBranchName(){
     echo "$(eval "git symbolic-ref HEAD | sed 's@^refs/heads/@@'")"
 }
 
+#resets to the default branch, and pulls
+function reset(){
+    eval "git reset --hard origin ${getDefaultBranchName}"
+    #eval "git pull"
+}
+
 gitRoot="C:\git";
 eval cd \"$gitRoot\";
 for d in */ ; do 
     eval cd \"$d\";
 	if [ -d .git ]; then
 		echo "Resetting repo: ${d%/}";
-        eval "git reset --hard origin/${getDefaultBranchName}"&
+        reset &
 	fi
     cd ..; 
 done 
