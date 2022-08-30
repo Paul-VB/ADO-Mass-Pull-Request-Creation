a319c6996f2f7f9c46a7443caf434ce6fc92590d
Paul Vanden Broeck <I823914@ngic.com>
2022-08-30 09:42:19 -0400

    improving the resetHardAllRepos script

--- Other-Handy-Scripts/resetHardAllRepos.sh
+++ Other-Handy-Scripts/resetHardAllRepos.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
+
+#get the default branch name of the current repository
+function getDefaultBranchName(){
+    echo "$(eval "git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@'")"
+}
+
+#get the current branch name of the current repository
+function getCurrentBranchName(){
+    echo "$(eval "git symbolic-ref HEAD | sed 's@^refs/heads/@@'")"
+}
+
 gitRoot="C:\git";
 eval cd \"$gitRoot\";
 for d in */ ; do 
     eval cd \"$d\";
 	if [ -d .git ]; then
 		echo "Resetting repo: ${d%/}";
-        eval "git reset --hard refs/remotes/origin/HEAD"&
+        eval "git reset --hard origin/${getDefaultBranchName}"&
 	fi
     cd ..; 
 done 
